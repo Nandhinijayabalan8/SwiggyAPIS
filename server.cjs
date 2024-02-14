78c6aac6dff6fe5db4c18d29605f18789885af2f
Nandhinijayabalan8 <nandhinij569@gmail.com>
2024-02-14 14:26:49 +0530

    delete api

--- server.cjs
+++ server.cjs
@@ -58,6 +58,30 @@ app.get('/get-restaurant-details', async function(request, response) {
     }
 })
 
+app.delete('/delete-restaurant-detail/:id', async function(request, response) {
+    try {
+        const restaurant = await Restaurant.findById(request.params.id)
+        if(restaurant) {
+            await Restaurant.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "deleted successfully"
+            })
+        } else { //restaurant : null
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "entry not found"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete",
+            "error" : error
+        })
+    }
+}) 
+
 
 
 
